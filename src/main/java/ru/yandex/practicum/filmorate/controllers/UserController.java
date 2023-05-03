7f1cd3599a9403b231c7a6e0aa05dbb01f173f75
lemfy <117842031+lemfy@users.noreply.github.com>
2023-05-03 18:37:21 +0300

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -0,0 +1,58 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    int id = 1;
+    public Map<String, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> allUsers() {
+        return users.values();
+    }
+
+    @PostMapping
+    public void create(@RequestBody @Valid User user) {
+        validate(user);
+        user.setId(id++);
+        users.put(user.getEmail(), user);
+    }
+
+    @PutMapping
+    public User change(@RequestBody @Valid User user) {
+        validate(user);
+        user.setId(user.getId());
+        users.put(user.getEmail(), user);
+        return user;
+    }
+
+    private void validate(User user) {
+        if ((user.getEmail().isBlank() || !user.getEmail().contains("@"))) {
+            throw new ValidationException("Имейл не может быть пустым или не содержать символ @");
+        }
+        if (user.getLogin().isBlank() || user.getLogin().contains(" ")) {
+            throw new ValidationException("Название не может быть пустым или содержать пробелы");
+        }
+        if (user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        if (user.getBirthday().equals(LocalDate.now()) || user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Неверная дата рождения");
+        }
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private int id;
+    @NotBlank(message = "Имейл не может быть пустым")
+    @Email(message = "Невалидный Имейл")
+    private String email;
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "\\S+", message = "Не может содержать пробелы")
+    private String login;
+    private String name;
+    @Past
+    private LocalDate birthday;
+
+    public User(String email, String login, String name, LocalDate birthday) {
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
+}
