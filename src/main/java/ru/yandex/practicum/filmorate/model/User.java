acc00604087d05db9d047936e4f3d77bd57fbb0e
lemfy <117842031+lemfy@users.noreply.github.com>
2023-05-03 19:47:20 +0300

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -27,6 +27,7 @@ public class User {
         this.name = name;
         this.birthday = birthday;
     }
+
     public User(String email, String login, LocalDate birthday) {
         this.email = email;
         this.login = login;
