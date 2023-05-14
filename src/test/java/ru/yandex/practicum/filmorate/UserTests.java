81d50c46d55adec872bb6fbf25cd9a6dfcb36bc1
lemfy <117842031+lemfy@users.noreply.github.com>
2023-05-14 19:34:26 +0300

    Delete src directory

--- src/test/java/ru/yandex/practicum/filmorate/UserTests.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.filmorate;
-
-import ru.yandex.practicum.filmorate.controllers.UserController;
-import ru.yandex.practicum.filmorate.model.User;
-
-public class UserTests {
-    UserController userController;
-    User user;
-}
