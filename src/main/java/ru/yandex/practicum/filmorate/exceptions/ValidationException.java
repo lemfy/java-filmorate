c9596cfeca23c8a94cacbc91d348ab07d6cdd895
lemfy <117842031+lemfy@users.noreply.github.com>
2023-06-06 15:43:21 +0300

    Delete src/main directory

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String s) {
-        super(s);
-    }
-}
